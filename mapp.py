2a910f86fa50d3a78b93875c0bd6d0dd3526f300
codesareus <125232658+codesareus@users.noreply.github.com>
2025-03-05 15:26:16 -0600

    Update mapp.py

--- mapp.py
+++ mapp.py
@@ -4,9 +4,9 @@ import streamlit as st
 import datetime
 
 # Schwab API details
-CLIENT_ID = "your_api_key_here"
-REDIRECT_URI = "your_redirect_uri_here"
-REFRESH_TOKEN = "your_refresh_token_here"
+CLIENT_ID = "4nApHqYZJnYndGYnQxXGn4pAdxDQ48Gi"
+REDIRECT_URI = "https://api.schwabapi.com/marketdata/v1"
+REFRESH_TOKEN = "3kBzb6yaQZ6tm3vM"
 ACCESS_TOKEN_URL = "https://api.schwabapi.com/v1/oauth2/token"
 PRICE_HISTORY_URL = "https://api.schwabapi.com/v1/marketdata/{symbol}/pricehistory"
 
